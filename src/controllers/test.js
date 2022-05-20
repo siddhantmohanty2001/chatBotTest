adc24bda449b9ff2616d5634a8139058b0b9aa97
Siddhant Mohanty <siddhantmohanty2001@gmail.com>
2022-05-21 00:35:58 +0530

    testing..

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -5,91 +5,89 @@ const { leadGenaratedLogs } = require("../utils/logger");
 const { mailComposeForSalesTeam } = require("../utils/mailComposer");
 const { sendMail } = require("../utils/mailer");
 const { slotData } = require('../utils/supporter')
+function extractEmails (text) {
+	return text.match(/([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9_-]+)/gi);
+  }
+  
 let contactController = {
 	
-	test: async (req, res) => {
+	contactUs: async (req, res) => {
 		let conversationData = req.body.conversationData;
-		// console.log(req.body);
-		// console.log(conversationData.slotValues);
-		let slotValues=conversationData.slotValues;
-		console.log("slot values here",slotValues);
+		console.log(req.body);
+		console.log("*****slotValues******",JSON.stringify(conversationData.slotValues));
+		let nameMessage=conversationData.userMessage;
+		const email=JSON.stringify(conversationData.slotValues.email);
+		console.log("*******EMAIL*****",email)
+		let name='';
+		console.log(email);
+		console.log("user message for name here",nameMessage);
 		try {
 
 			// // Ask email conditions 
-			// if(slotValues.given-name.listValue.values.length()>0){
-			// 	let slotsData = { isSlotGiven: false, slotsAnswered: [] };
-		
-			// 			responseObject = [
+			
+			if(!email)
+			{
+				var responseObject = [];
+            
+			if (!Array.isArray(conversationData.slotsAnswered)) conversationData.slotsAnswered = [];
+			let slotsData = { isSlotGiven: false, slotsAnswered: [] };
+
+						responseObject = [
                             
             
                                 
-            //                         {
-            //                             conditions: [
-            //                                 {
-            //                                     "conditionType": "askEmail",
-            //                                     "conditionValue": [conversationData]
-            //                                 }
-            //                             ],
-                                        
-            //                         }
-                                
-                            
-            //             ]
-						
-			// // console.dir(responseObject, { depth: null, colors: true });
-			// let result = integrator.responseCreater(responseObject);
-			// return res.status(result.statusCode).json(result);
-			// }
-			// //confirm name and email
-			// else if(slotValues.email.listValue.values.length()>0)
-			// {
-			// 	let slotsData = { isSlotGiven: false, slotsAnswered: [] };
-		
-			// 			responseObject = [
-                            
-            
-                                
-            //                         {
-            //                             conditions: [
-            //                                 {
-            //                                     "conditionType": "nameEmailConfirm",
-            //                                     "conditionValue": [conversationData]
-            //                                 }
-            //                             ],
+                                    {
+                                        conditions: [
+                                            {
+                                                "conditionType": "askEmail",
+                                                "conditionValue": [conversationData]
+                                            }
+                                        ],
+										replaceMentValues: [
+                                          
+                                        ]
                                        
-            //                         }
+                                    }
                                 
                             
-            //             ]
+                        ]
 						
 			// console.dir(responseObject, { depth: null, colors: true });
-			// let result = integrator.responseCreater(responseObject);
-			// return res.status(result.statusCode).json(result);
-			// }
-			// else{
-			// 	let slotsData = { isSlotGiven: false, slotsAnswered: [] };
-		
-			// 			responseObject = [
+			let result = integrator.responseCreater(responseObject);
+			return res.status(result.statusCode).json(result);
+			}
+			else if(email) {
+				var responseObject = [];
+            
+			if (!Array.isArray(conversationData.slotsAnswered)) conversationData.slotsAnswered = [];
+			let slotsData = { isSlotGiven: false, slotsAnswered: [] };
+
+						responseObject = [
                             
             
                                 
-            //                         {
-            //                             conditions: [
-            //                                 {
-            //                                     "conditionType": "askName",
-            //                                     "conditionValue": [conversationData]
-            //                                 }
-            //                             ]
-            //                         }
+                                    {
+                                        conditions: [
+                                            {
+                                                "conditionType": "askName",
+                                                "conditionValue": [conversationData]
+                                            }
+                                        ],
+										replaceMentValues: [
+										
+                                        ]
+                                    }
                                 
                             
-            //             ]
-						
-			// // console.dir(responseObject, { depth: null, colors: true });
-			// let result = integrator.responseCreater(responseObject);
-			// return res.status(result.statusCode).json(result);
-			// }
-			let responseObject = [];
+                        ]
+			name=JSON.stringify(conversationData.slotValues.given-name)	
+			console.log("*****NAME IS****",name);		
+			// console.dir(responseObject, { depth: null, colors: true });
+			let result = integrator.responseCreater(responseObject);
+			return res.status(result.statusCode).json(result);
+			}
+			
+			var responseObject = [];
             
 			if (!Array.isArray(conversationData.slotsAnswered)) conversationData.slotsAnswered = [];
 			let slotsData = { isSlotGiven: false, slotsAnswered: [] };
